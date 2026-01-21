88f776f4e1b7884433aab415e4fbd1a2fd606de6
Puspalatha_S <pushpa22042005@gmail.com>
2026-01-21 16:29:31 +0530

    Delete UserManagementApi directory

--- UserManagementApi/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-
-namespace UserManagementAPI.Models;
-
-public class AppUser
-{
-    public int Id { get; set; }        // 0 allowed
-    public string Name { get; set; } = "";
-    public string Email { get; set; } = "";
-}
