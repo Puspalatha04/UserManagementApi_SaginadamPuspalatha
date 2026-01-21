f66d979d274307f79234f0b563ffdcfe50998333
Puspalatha_S <pushpa22042005@gmail.com>
2026-01-21 16:30:50 +0530

    Add files via upload

---
+++ UserManagementApi/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,23 @@
+
+namespace UserManagementAPI.Middleware;
+
+public class ErrorHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "Internal server error."
+            });
+        }
+    }
+}
